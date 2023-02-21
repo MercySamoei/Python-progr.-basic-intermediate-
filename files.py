4af6727d1bc41111a1d0a4bf97a1fc7bd5cb2b37
Mercy Samoei <59794245+MercySamoei@users.noreply.github.com>
2023-02-21 16:31:42 +0300

    Add files via upload

---
+++ files.py
@@ -0,0 +1,29 @@
+def reads(file_name):
+ open('try.txt','r') as ['dictionary'['book','title','author','ISBN','price']]
+return reads
+
+def search_by_name(author_name):
+    for book in books:
+    if author name== author name:
+        return search_by_name
+    
+def search_by_title(title):
+    for book in books:
+    if title== title:
+        return search_by_title
+    
+def search_by_ISBN(ISBN):
+    for book in books:
+    if ISBN== ISBN:
+        return search_by_ISBN
+    
+def search_by_price(price):
+    for book in books:
+        max price=float(input("Maximum price is",price))
+        min price=float(input("Minimum price is",price))
+    if price== price:
+        return search_by_price
+    
+def general_search(search):
+    search [search_by_price or search_by_ISBN or search_by_name]
+    return books
