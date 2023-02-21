6059fd5ff29338fbca90f8dcae4afcb852ff72a1
Mercy Samoei <59794245+MercySamoei@users.noreply.github.com>
2023-02-21 23:38:59 +0300

    Update palin.py

--- palin.py
+++ palin.py
@@ -1,30 +1,19 @@
-#def is_palindrome(text):
-    #return text == text[::-1]
+def is_palindrome(text):
+    return text == text[::-1]
 
-#with open('dictionary.txt', 'r') as file:
-    #text = file.read()
+with open('try.txt', 'r') as file:
+    text = file.read()
 
-#words = text.split(\n)
+words = text.split("\n")
 
-#palindromes = []
-#for word in words:
-    #if is_palindrome(word):
-       # palindromes.append(word)
-
-#if palindromes:
-    #for palindrome in palindromes:
-       # print(palindrome)
-
-
-words=[]
-
-with open('Dictionary.txt', "r") as dictwords:
-    for i in dictwords.read().splitlines():
-        words.append(i)
-for x in words:
-    if x == x[::-1]:
-        print(x)
-
+palindromes = []
+for word in words:
+    if is_palindrome(word):
+        palindromes.append(word)
+print(palindromes)
+if palindromes:
+    for palindrome in palindromes:
+        print(palindrome)
 
 
 
